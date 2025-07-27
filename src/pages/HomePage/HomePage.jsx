7469c7dc2bb7b8e2404cdd8c0522e55f8adc8422
IgorMedvedenko <noveciti0@gmail.com>
2025-07-27 23:45:27 +0300

    aboutus

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import AboutUs from "../../components/AboutUs/AboutUs.jsx";
+
 export default function HomePage() {
   return (
     <>
       <div>
         <h1>Welcome to the Harmoniq App!</h1>
       </div>
+      <AboutUs />
     </>
   );
 }
