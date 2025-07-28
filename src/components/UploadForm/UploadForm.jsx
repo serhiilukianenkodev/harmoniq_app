b2bcd1288f2455ab26946878a966c52e026f0296
maryna33309 <68459246+marusya2299@users.noreply.github.com>
2025-07-28 17:50:54 +0200

    Media Styles

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,4 +1,5 @@
 import css from "./UploadForm.module.css";
+
 const UploadForm = () => {
     /*
     const state = getState();
@@ -12,7 +13,12 @@ const UploadForm = () => {
 
             <label class={css.labelUploadForm}>
                 <input type="file" accept="image/*" class={css.inputUploadForm} />
-                <div class={css.circleButtonUploadForm}></div>
+                <div class={css.circleButtonUploadForm}>
+                    <svg class={css.iconCircleButtonUploadForm}>
+                        <use xlinkHref='/icons/sprite.svg#eye'></use>
+                    </svg>
+
+                </div>
             </label>
 
             <button className={css.buttonUploadForm}>Save</button>
