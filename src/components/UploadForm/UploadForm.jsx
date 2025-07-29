bbc69492a99addb3cbe84016c0dfb25033a6f3e9
maryna33309 <68459246+marusya2299@users.noreply.github.com>
2025-07-29 14:04:43 +0200

    Some Upload Logic

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,27 +1,49 @@
 import css from "./UploadForm.module.css";
+import { useState } from 'react';
 
-const UploadForm = () => {
-    /*
-    const state = getState();
-    const userId = state.auth.user._id;
-    const token = state.auth.token;
-    */
+const UploadForm = ({ image, setImage, file, setFile }) => {
+    const handleImageChange = (e) => {
+        const selectedFile = e.target.files[0];
+        if (!selectedFile) return;
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setImage(reader.result);
+        };
+        reader.readAsDataURL(selectedFile);
+
+        setFile(selectedFile);
+    };
 
     return (
         <div className={css.containerUploadForm}>
             <h2 className={css.titleUploadForm}>Upload your photo</h2>
 
-            <label class={css.labelUploadForm}>
-                <input type="file" accept="image/*" class={css.inputUploadForm} />
-                <div class={css.circleButtonUploadForm}>
-                    <svg class={css.iconCircleButtonUploadForm}>
-                        <use xlinkHref='/icons/sprite.svg#eye'></use>
+            <label className={css.labelUploadForm}>
+                <input 
+                    type="file" 
+                    accept="image/*" 
+                    className={css.inputUploadForm} 
+                    onChange={handleImageChange}
+                />
+                <div 
+                    className={css.circleButtonUploadForm}
+                    style={image ? { backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' } : {}}
+                > 
+                {!image && (
+                    <svg className={css.iconCircleButtonUploadForm}>
+                    <use xlinkHref='/icons/sprite.svg#eye'></use>
                     </svg>
-
+                )}
                 </div>
             </label>
 
-            <button className={css.buttonUploadForm}>Save</button>
+            <button 
+            className={css.buttonUploadForm}
+            disabled={!file}
+            >
+                Save
+            </button>
         </div>
     );
 };
