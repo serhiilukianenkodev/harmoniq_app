60e0cb3686a6d4544b284f102e2417a8b2c886ef
AliceMozyrko <mozyrkoalisa@gmail.com>
2025-07-30 13:26:48 +0300

    update

--- src/pages/CreateArticlePage/CreateArticlePage.jsx
+++ src/pages/CreateArticlePage/CreateArticlePage.jsx
@@ -1,5 +1,4 @@
-import Container from "../../components/Container/Container";
-import CreateArticleForm from "../../components/createArticleForm/createArticleForm";
+import CreateArticleForm from "../../components/CreateArticleForm/CreateArticleForm";
 import css from "./CreateArticlePage.module.css"
 
 
