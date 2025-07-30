96e327343f5d63c4bea39a77768570238646bc54
Yurii <evseevyrij90@gmail.com>
2025-07-30 17:39:18 +0200

    Add modal logic for creating articles and logout confirmation

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,18 @@
-import LoginForm from "../../components/LoginForm/LoginForm";
-import { NavLink } from "react-router-dom";
-import css from "./LoginPage.module.css"
+import LoginForm from '../../components/LoginForm/LoginForm';
+import { NavLink } from 'react-router-dom';
+import css from './LoginPage.module.css';
 
 export default function LoginPage() {
   return (
-      <div className={css.container}>
-        <h1 className={css.title}>Login</h1>
-        <LoginForm />
-        <p className={css.txt}>Dont have an account? <NavLink className={css.regLink} to="/register">Register</NavLink></p>
-        </div>
+    <div className={css.container}>
+      <h1 className={css.title}>Login</h1>
+      <LoginForm />
+      <p className={css.txt}>
+        Dont have an account?{' '}
+        <NavLink className={css.regLink} to="/register">
+          Register
+        </NavLink>
+      </p>
+    </div>
   );
 }
